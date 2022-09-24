4bd240f486d4c72fbc861fd31322390ed6d167a6
juanmera01 <Uo277406@uniovi.es>
2022-09-25 00:14:35 +0200

    Add search and filter UI options (without login due to possible migration to spring architecture)

--- app/src/main/java/com/main/es/sellverse/home/HomeFragment.java
+++ app/src/main/java/com/main/es/sellverse/home/HomeFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.main.es.sellverse.R;
@@ -55,9 +57,9 @@ public class HomeFragment extends Fragment {
                 R.drawable.btn_home, R.drawable.btn_home,R.drawable.btn_home};
 
         GridAdapter gridAdapter = new GridAdapter(getActivity(),auctionData,auctionImages);
-        binding.gridView.setAdapter(gridAdapter);
+        binding.gridViewCatalog.setAdapter(gridAdapter);
 
-        binding.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        binding.gridViewCatalog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
@@ -65,7 +67,22 @@ public class HomeFragment extends Fragment {
 
             }
         });
+
+        binding.gridViewCatalog.setOnScrollChangeListener(new View.OnScrollChangeListener(){
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY){
+                if(oldScrollY > 0) {
+                    binding.layoutSearch.removeAllViews();
+                    System.out.println("baja");
+                }else if(oldScrollY < 0) {
+                    binding.layoutSearch.setVisibility(View.VISIBLE);
+                    System.out.println("sube");
+                }
+            }
+        });
     }
+
+
 
 
 }
