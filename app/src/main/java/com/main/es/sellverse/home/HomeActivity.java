15ed388ae742d290387066e735887b7ab5579ad2
Sergio Salgueiro Monforte <UO276843@uniovi.es>
2022-09-24 16:48:28 +0200

    Changes of add

--- app/src/main/java/com/main/es/sellverse/home/HomeActivity.java
+++ app/src/main/java/com/main/es/sellverse/home/HomeActivity.java
@@ -4,13 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.main.es.sellverse.R;
+import com.main.es.sellverse.add.AddPublicationActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,10 +33,23 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Set up the navigation menu
+     */
     private void setUpFragments() {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        View add = findViewById(R.id.add_icon);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddActivity();
+            }
+        });
 
+        //this is just for add icon because it needs to open a new activity
+
+        //this is for the other one because it needs to open a fragment
         bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
@@ -46,9 +62,6 @@ public class HomeActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,yourBidsFragment).commit();
                         break;
 
-                    case R.id.add_icon:
-                       openAddActivity();
-                        break;
 
                     case R.id.profile_icon:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
@@ -59,13 +72,20 @@ public class HomeActivity extends AppCompatActivity {
                         break;
                 }
 
-                }
-            });
-        }
+            }
+        });
+    }
 
+    /**
+     * Opens the add activity
+     */
     private void openAddActivity() {
+        Intent intent = new Intent(this, AddPublicationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.up_in,R.anim.up_out);
 
     }
+
 }
 
 
