fb3c179a30224fdf50243d8207cb71e3938e8afe
Sergio Salgueiro Monforte <UO276843@uniovi.es>
2022-09-26 23:05:45 +0200

    Save Copy

--- app/src/main/java/com/main/es/sellverse/add/AddPublicationActivity.java
+++ app/src/main/java/com/main/es/sellverse/add/AddPublicationActivity.java
@@ -4,17 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.main.es.sellverse.R;
 
 
-public class AddPublicationActivity extends AppCompatActivity {
+public class AddPublicationActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_publication);
+        setUpSpinner();
 
+    }
+
+    private void setUpSpinner() {
+        AutoCompleteTextView spinner = findViewById(R.id.coinSpinner);
+        spinner.setText(getResources().getStringArray(R.array.coins)[0]);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.coins, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text= adapterView.getItemAtPosition(i).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
