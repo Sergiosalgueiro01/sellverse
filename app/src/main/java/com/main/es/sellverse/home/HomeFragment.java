17846236b37da3ce1941592a965c88ddfec5f348
juanmera01 <Uo277406@uniovi.es>
2022-10-12 12:33:30 +0200

    Toolbar for auctions catalog

--- app/src/main/java/com/main/es/sellverse/home/HomeFragment.java
+++ app/src/main/java/com/main/es/sellverse/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -51,45 +52,43 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.view = view;
-        EditText searchText = view.findViewById(R.id.editSearch);
-        searchText.setOnClickListener(new View.OnClickListener() {
+        setUpAuctionCatalog();
+        ActionMenuItemView filterText = view.findViewById(R.id.filters);
+        filterText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setUpSearchActivity();
+                setUpFilterActivity();
             }
         });
-        setUpAuctionCatalog();
+        ActionMenuItemView searchSimbol = view.findViewById(R.id.search);
+        searchSimbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setUpFilterActivity();
+            }
+        });
+
+
         //utiliza aqui el view hijo puta, no existe en fragmentos el findviewById pero puedes hacer el view.findViewbyId
     }
 
     private void setUpAuctionCatalog() {
-
-        new RetrieveAuctionsTask().execute(
-                this
-        );
-
+        new RetrieveAuctionsTask().execute(this);
     }
 
     public void setUpGrid(List<Auction> auctions){
-
-
-
         GridAdapter gridAdapter = new GridAdapter(requireActivity(), auctions);
         GridView g =  requireActivity().findViewById(R.id.gridViewCatalog);
         g.setAdapter(gridAdapter);
-
         g.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
                 Toast.makeText(requireActivity(),"You Clicked on "+ auctions.get(position).getTitle(),Toast.LENGTH_SHORT).show();
-
-
             }
         });
     }
 
-    private void setUpSearchActivity() {
+    private void setUpFilterActivity() {
         Intent intent = new Intent(requireActivity(), SearchActivity.class);
         startActivity(intent);
     }
