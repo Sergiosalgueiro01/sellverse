52b585ab2e7fc3f951c8bcf5c02aef57b5d05209
Sergio Salgueiro Monforte <UO276843@uniovi.es>
2022-12-09 20:32:24 +0100

    notifications

---
+++ app/src/main/java/com/main/es/sellverse/notifications/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.main.es.sellverse.notifications;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
